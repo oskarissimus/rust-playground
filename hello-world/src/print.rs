f358e426fe1518ca74fd5dd2ea62693d2b51127a
Oskar Korczak <oskar.korczak@igoriatrade.com>
2022-01-16 23:12:58 +0100

    more formatting

--- hello-world/src/print.rs
+++ hello-world/src/print.rs
@@ -1,4 +1,15 @@
 pub fn run() {
     // print to console
     println!("{} is from {}", "oskar", "wwa");
+    //positional args
+    println!(
+        "{0} is from {1} and {0} likes {2}",
+        "oskar", "wwa", "vaping"
+    );
+    // named args
+    println!(
+        "{name} likes {activity}",
+        name = "oskar",
+        activity = "vaping"
+    )
 }
